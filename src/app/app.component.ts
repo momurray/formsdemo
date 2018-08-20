00379e7e536790ee26dfa722d30c83c3cee66a1d
Mark Murray <m.murray@pobox.com>
2018-08-20 22:46:45 +0100

    Added some helper code to see what was going on.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '../../node_modules/@angular/forms';
+import { FormlyFieldConfig } from '../../node_modules/@ngx-formly/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
@@ -11,6 +13,7 @@ export class AppComponent implements OnInit {
     model: any;
     fields: any;
 
+    changePasswordModel = { name: 'Change Password', id: "1744332", newPassword: 'Reward2018!', oldPassword: 'Reward2019!', confirmPassword: 'Reward2019!' };
     changePasswordFields: FormlyFieldConfig[] = [
         {
             key: 'name',
@@ -63,9 +66,8 @@ export class AppComponent implements OnInit {
             }
         }
     ];
-    changePasswordModel = { name: 'Change Password', id: "1744332", newPassword: 'Reward2018!', oldPassword: 'Reward2019!', confirmPassword: 'Reward2019!' };
 
-
+    changeNomineeModel = { name: 'Change Nominee', id: "732332", oldName: 'Mark Murray', newName: 'Shaniece Nesbitt' }
     changeNomineeFields: FormlyFieldConfig[] = [
         {
             key: 'name',
@@ -87,7 +89,7 @@ export class AppComponent implements OnInit {
                 'templateOptions.disabled': 'true'
             }
         },
-           {
+        {
             key: 'oldName',
             type: 'input',
             templateOptions: {
@@ -104,11 +106,57 @@ export class AppComponent implements OnInit {
             }
         }
     ];
-    changeNomineeModel = { name: 'Change Nominee', id: "732332", oldName: 'Mark Murray', newName: 'Shaniece Nesbitt' }
 
-    redemptionFields: FormlyFieldConfig[] = [{
-
-    }];
+    redemptionModel = { name: 'Redemption', id: "1773432", retailer: 'marksandspencer', redeemCode: '17732', redeemValue: 25 }
+    redemptionFields: FormlyFieldConfig[] = [
+        {
+            key: 'name',
+            type: 'input',
+            templateOptions: {
+                label: 'Template Name',
+            },
+            expressionProperties: {
+                'templateOptions.disabled': 'true'
+            }
+        },
+        {
+            key: 'id',
+            type: 'input',
+            templateOptions: {
+                label: 'SFD Template Id',
+            },
+            expressionProperties: {
+                'templateOptions.disabled': 'true'
+            }
+        },
+        {
+            key: 'retailer',
+            type: 'input',
+            templateOptions: {
+                label: 'Retailer',
+                required: true
+            }
+        },
+        {
+            key: 'redeemCode',
+            type: 'input',
+            templateOptions: {
+                label: 'Redemption Code',
+                required: true
+            }
+        },
+        {
+            key: 'redeemValue',
+            type: 'input',
+            templateOptions: {
+                label: 'Redemption Value (min £5, max £75)',
+                type: 'number',
+                min: 5,
+                max: 75,
+                required: true,
+            },
+        }
+    ];
 
     ngOnInit() {
         this.model = this.changePasswordModel;
@@ -132,6 +180,8 @@ export class AppComponent implements OnInit {
                 break;
 
             case 'redemption':
+                this.model = this.redemptionModel;
+                this.fields = this.redemptionFields;
                 break;
 
             default:
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormlyModule} from '@ngx-formly/core';
+import {FormlyBootstrapModule} from '@ngx-formly/bootstrap';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
